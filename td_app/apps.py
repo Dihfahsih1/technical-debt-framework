95864851eeacb4aee41031e002c09190e0fc830a
Dihfahsih1 <dihfahsihm@gmail.com>
2024-01-02 23:35:26 +0300

    rename the app to something more meaningful

--- td_app/apps.py
+++ td_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TdConfig(AppConfig):
+class TdAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'td'
+    name = 'td_app'
